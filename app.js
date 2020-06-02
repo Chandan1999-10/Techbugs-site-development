30a787df118ca60d72facf33e6a3112be2c500a6
Chandan1999-10 <58650645+Chandan1999-10@users.noreply.github.com>
2020-06-02 17:09:48 +0530

    blog enabled

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-const express=require("express");
-const app=express();
-const bodyParser=require("body-parser");
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+const posts = [];
 
-app.get("/",function(req,res){
-	res.render('home');})
+app.get("/", function(req, res) {
+  res.render('home',{
+  	posts:posts
+
+    });
+})
+app.get("/compose", function(req, res) {
+  res.render('compose');
+})
+app.post("/compose",function(req,res){
+  post={
+    title:req.body.val1,
+    content:req.body.val2,
+  }
+  posts.push(post);
+  res.redirect("/");
+})
 
 
 
 
 
-app.listen(3000,function(argument) {
-	console.log("server is running on port 3000")
+
+app.listen(3000, function(argument) {
+  console.log("server is running on port 3000")
 })
